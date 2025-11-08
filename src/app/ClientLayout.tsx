f62e49407c35fe3dc07de1d65a88f54fc00238d1
priya05122000 <npriyanagendran2000@gmail.com>
2025-11-08 11:44:05 +0530

    fixes

---
+++ src/app/ClientLayout.tsx
@@ -0,0 +1,27 @@
+"use client";
+
+import React, { ReactNode } from "react";
+
+import Navbar from "@/components/Navbar";
+import AOSInit from "@/components/AOSInit";
+// import Footer from "@/components/Footer";
+
+interface ClientLayoutProps {
+  children: ReactNode;
+}
+
+const ClientLayout: React.FC<ClientLayoutProps> = ({ children }) => {
+  return (
+    <>
+      <AOSInit />
+
+      <Navbar />
+
+      <main className={`relative z-10 pt-20 }`}>{children}</main>
+
+      {/* <Footer /> */}
+    </>
+  );
+};
+
+export default ClientLayout;
