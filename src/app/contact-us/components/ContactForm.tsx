4c7c3f20e32d5b571a0e6b0b99a0d2e681747fcb
priya05122000 <npriyanagendran2000@gmail.com>
2025-11-22 13:04:50 +0530

    fixes

--- src/app/contact-us/components/ContactForm.tsx
+++ src/app/contact-us/components/ContactForm.tsx
@@ -88,54 +88,54 @@ const ContactForm = () => {
 
   return (
     <Section className="py-10 sm:py-20">
-      <div className="contact-form grid grid-cols-1  xl:grid-cols-[1fr_3fr] gap-20">
+      <div className="contact-form grid grid-cols-1   xl:grid-cols-[1fr_3fr] gap-10 xl:gap-20">
         <div className="text-end flex flex-col justify-center">
           <Heading level={3} className="font-bold mb-4 uppercase text-(--teal)">
             Fill our enquiry form today
           </Heading>
           <Paragraph
-            size="lg"
-            className="mt-8 mb-6 flex items-center gap-2 justify-end"
+            size="base"
+            className="mt-8 mb-6 flex items-end gap-2 justify-end"
           >
             Facebook{" "}
-            <span className="ml-10 text-3xl text-(--teal)">
+            <span className="ml-8 text-3xl text-(--teal)">
               <FaFacebookF />
             </span>
           </Paragraph>
           <Paragraph
-            size="lg"
-            className=" flex mb-6 items-center gap-2 justify-end"
+            size="base"
+            className=" flex mb-6 items-end gap-2 justify-end"
           >
             Instagram{" "}
-            <span className="ml-10 text-3xl text-(--teal)">
+            <span className="ml-8 text-3xl text-(--teal)">
               <FaInstagramSquare />
             </span>
           </Paragraph>
-          <Paragraph size="lg" className=" flex items-center gap-2 justify-end">
+          <Paragraph size="base" className=" flex items-end gap-2 justify-end">
             LinkedIn{" "}
-            <span className="ml-10 text-3xl text-(--teal)">
+            <span className="ml-8 text-3xl text-(--teal)">
               <FaLinkedin />
             </span>
           </Paragraph>
         </div>
         <div className=" flex w-full flex-col items-end">
-          <div className="bg-(--teal)/80 backdrop-blur-md relative p-10 w-4/5 flex justify-end ">
-            <div className="absolute top-1/2 left-0 -translate-x-1/2 -translate-y-1/2 w-[400px] h-[80%] ">
+          <div className="bg-(--teal) rounded-md sm:bg-(--teal)/80 backdrop-blur-md relative p-10  sm:w-3/4 xl:w-4/5 flex justify-end ">
+            <div className="absolute top-1/2 left-0 -translate-x-1/2 -translate-y-1/2 w-[320px] lg:w-[400px] h-[80%] hidden sm:block">
               <div className="relative  w-full h-full">
-                <div className="absolute  right-0 bottom-0 w-[calc(100%-30px)] h-[calc(100%-30px)]    bg-(--teal)"></div>
+                <div className="absolute  right-0 bottom-0 w-[calc(100%-30px)] h-[calc(100%-30px)] rounded-md   bg-(--teal)"></div>
                 <div className="absolute  left-0 top-0 w-[calc(100%-30px)] h-[calc(100%-30px)]">
                   <Image
                     src="/contact/contactform.jpg"
                     alt="Decorative"
                     width={500}
                     height={500}
-                    className="object-cover w-full h-full"
+                    className="object-cover w-full h-full rounded-md"
                   />
                 </div>
               </div>
             </div>
 
-            <div className="w-2/3 flex  items-end">
+            <div className="sm:w-2/3 flex  items-end">
               <div className="w-full">
                 <div className="flex flex-col items-center mb-6">
                   <span className="text-6xl mb-2 text-white">
